039c4370ad06bc72013abb98176dd949bb15f09a
Alex <b514t014@gmail.com>
2024-01-26 23:05:24 +0200

    add chakra ui

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { store } from "./store/store.js";
 import { Provider } from "react-redux";
 import { Toaster } from "react-hot-toast";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
