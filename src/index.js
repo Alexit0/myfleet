db1541a02a65ebbcc4005d06f757cfa4969b6418
Alex <b514t014@gmail.com>
2024-01-26 23:24:23 +0200

    add default theme

--- src/index.js
+++ src/index.js
@@ -5,23 +5,35 @@ import App from "./App";
 import { store } from "./store/store.js";
 import { Provider } from "react-redux";
 import { Toaster } from "react-hot-toast";
+import { ChakraProvider, extendTheme, theme } from "@chakra-ui/react";
 
+// const theme = extendTheme({
+//   colors: {
+//     brand: {
+//       100: "#f7fafc",
+//       // ...
+//       900: "#1a202c",
+//     },
+//   },
+// });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <App />
-    <Toaster
-      position="top-left"
-      reverseOrder={false}
-      toastOptions={{
-        success: {
-          iconTheme: {
-            primary: "antiquewhite",
-            secondary: "#151d2c",
+    <ChakraProvider theme={theme}>
+      <Toaster
+        position="top-left"
+        reverseOrder={false}
+        toastOptions={{
+          success: {
+            iconTheme: {
+              primary: "antiquewhite",
+              secondary: "#151d2c",
+            },
           },
-        },
-      }}
-    />
+        }}
+      />
+    </ChakraProvider>
   </Provider>
 );
